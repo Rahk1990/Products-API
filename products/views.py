aa542f8c2a4deece38c80c17e0ca3945c306c5e1
rahk1990 <rahkem.ellis@gmail.com>
2022-02-15 18:01:04 -0500

    modified foremntioned file and added urls.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .serializers import ProductSerializer
+from .models import Product
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
+
+@api_view(['GET'])
+def product_list(request):
+    return Response('ok')   
