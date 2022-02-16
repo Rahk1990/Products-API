8315885f417d4226ccc59f9161aa12aa3b94cb5f
rahk1990 <rahkem.ellis@gmail.com>
2022-02-16 14:45:16 -0500

    Updated views to enable 'DELETE' function for post request.

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 
+from os import stat
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
@@ -31,22 +32,25 @@ def product_list(request):
         serializer.save()
         return Response(serializer.data, status.HTTP_201_CREATED)
     
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, pk):
    
-   product = get_object_or_404(Product, pk=pk)
-   
-   if request.method == 'GET':
-        
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-        
-   elif request.method == 'PUT':
-        serializer = ProductSerializer(product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+    product = get_object_or_404(Product, pk=pk)
+    
+    if request.method == 'GET':
+            
+            serializer = ProductSerializer(product)
+            return Response(serializer.data)
+            
+    elif request.method == 'PUT':
+            serializer = ProductSerializer(product, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
 
+    elif request.method == 'DELETE':
+            product.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
     # except Product.DoesNotExist:
     #     return Response(status=status.HTTP_400_BAD_REQUEST)
